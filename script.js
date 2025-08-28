eeec6449a5ab1d517daf18c07c718c5ef1d1d8c8
takashi74 <takashi.okawa@icloud.com>
2025-08-29 04:14:33 +0900

    purchase

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const REDIRECT_URI = "https://auth.streamtech.cloud/callback";
 
 // 「視聴権利の取得」ボタンのイベントリスナー
 document.getElementById('get-rights-btn').addEventListener('click', () => {
-    // ユーザーをPretixの認可エンドポイントにリダイレクト
     const pretixAuthorizeUrl = new URL('https://pretix.streamtech.cloud/pyconjp/oauth2/v1/authorize');
     pretixAuthorizeUrl.searchParams.set('client_id', CLIENT_ID);
     pretixAuthorizeUrl.searchParams.set('response_type', 'code');
@@ -14,7 +13,7 @@ document.getElementById('get-rights-btn').addEventListener('click', () => {
     window.location.href = pretixAuthorizeUrl.toString();
 });
 
-// URLから認証コードを取得し、動画を再生
+// URLから認証コードを取得
 const urlParams = new URLSearchParams(window.location.search);
 const authCode = urlParams.get('code');
 
@@ -24,6 +23,20 @@ const videoUrl = "https://wowza.jst-lab.com/okawa_live_test/video1/playlist.m3u8
 if (authCode) {
     console.log("認証コードを取得しました:", authCode);
     
+    // チケット購入ボタンと視聴権利ボタンを無効化する関数
+    function disableButtons() {
+        const primaryBtn = document.querySelector('.btn-primary');
+        const secondaryBtn = document.querySelector('.btn-secondary');
+
+        // ボタンを無効化
+        primaryBtn.style.pointerEvents = 'none';
+        primaryBtn.style.opacity = '0.5';
+        primaryBtn.href = '#';
+
+        secondaryBtn.style.pointerEvents = 'none';
+        secondaryBtn.style.opacity = '0.5';
+    }
+
     // HLS動画を初期化して再生する関数
     function playVideo(videoId, streamUrl) {
         const videoElement = document.getElementById(videoId);
@@ -32,9 +45,13 @@ if (authCode) {
             hls.loadSource(streamUrl);
             hls.attachMedia(videoElement);
             videoElement.play();
+            // 動画の再生が始まったらボタンを無効化
+            videoElement.addEventListener('playing', disableButtons, { once: true });
         } else if (videoElement.canPlayType('application/vnd.apple.mpegurl')) {
             videoElement.src = streamUrl;
             videoElement.play();
+            // 動画の再生が始まったらボタンを無効化
+            videoElement.addEventListener('playing', disableButtons, { once: true });
         }
     }
 
