6d0cf27793c3c6d98f0a423f9248f31327614a42
takashi74 <takashi.okawa@icloud.com>
2025-08-29 02:50:57 +0900

    vue init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const msg = ref('Hello, World!')
+</script>
+
+<style scoped>
+h1 {
+  text-align: center;
+  color: #333;
+  margin-top: 100px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './style.css' // デフォルトで生成されるスタイルシート
+
+createApp(App).mount('#app')
